8c38feb9a4c87d1fe3525acf1dcf17ab40f713ce
harshitha2481 <61981163+harshitha2481@users.noreply.github.com>
2020-06-10 15:29:23 +0530

    Enter

--- src/components/Places/Place.js
+++ src/components/Places/Place.js
@@ -11,7 +11,7 @@ const Place = ({ place }) => {
         <article className={styles.place}>
             <div className={styles.imgContainer}>
                 <Image fluid={mainImage} className={styles.img} alt="single place" />
-                <Link fade className={styles.link} to={`/places/${slug}`}>details</Link>
+                <Link fade className={styles.link} to={`/places/${slug}`}>Details</Link>
             </div>
             <div className={styles.footer}>
                 <h3>{name}</h3>
